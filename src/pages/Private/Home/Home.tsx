767eefb2b0c736d9234b807f5202e72242bd9211
Matheus Aires <matheus.aires@majovdev.com.br>
2023-09-30 15:42:22 -0300

    home page  setup

---
+++ src/pages/Private/Home/Home.tsx
@@ -0,0 +1,16 @@
+import { Typography } from "@mui/material";
+
+const Home = () => {
+  return (
+    <>
+      <Typography variant="h1" color="primary">
+        BBookshel
+      </Typography>
+      <Typography variant="h2" color="secondary">
+        Home
+      </Typography>
+    </>
+  );
+};
+
+export default Home;
