dc58985c1d2acaa1b0b27c5bb2747e1b9db7af53
Matheus Aires <matheus.aires@majovdev.com.br>
2023-10-01 19:02:20 -0300

    ToogleDrawer context

---
+++ src/context/ToggleDrawer/ToggleDrawer.tsx
@@ -0,0 +1,36 @@
+import { createContext, useState } from "react";
+
+interface SidebarContextType {
+  open: boolean;
+  toggleDrawer: VoidFunction;
+}
+
+const DEFAULT_VALUE = {
+  open: true,
+  toggleDrawer: () => {console.log('DEFAULT_VALUE')},
+};
+
+export const SidebarContext = createContext<SidebarContextType>(
+    DEFAULT_VALUE
+);
+
+interface ToggleDrawerProps {
+  children: React.ReactNode;
+}
+
+const ToggleDrawer = ({ children }: ToggleDrawerProps) => {
+  const [open, setOpen] = useState(true);
+
+  const toggleDrawer = () => {
+    console.log('toggleDrawer', open)
+    setOpen(!open);
+  };
+
+  return (
+    <SidebarContext.Provider value={{ open, toggleDrawer }}>
+      {children}
+    </SidebarContext.Provider>
+  );
+};
+
+export default ToggleDrawer;
