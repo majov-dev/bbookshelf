5c3ba862da8bbb843f61adeb92e7035520bfd2da
Matheus Aires <matheus.aires@majovdev.com.br>
2023-10-02 20:57:56 -0300

    delete reading

--- src/pages/Private/PreReading/PreReading.tsx
+++ src/pages/Private/PreReading/PreReading.tsx
@@ -1,12 +1,32 @@
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useLibary } from "../../../hooks/useLibary";
-import { Box, Button, Stack } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Modal,
+  Stack,
+  Typography,
+} from "@mui/material";
 import Book from "../../../components/Book/Book";
 import Description from "./components/Description/Description";
+import { useState } from "react";
+import { useUser } from "../../../hooks/userUser";
+import { Cancel } from "@mui/icons-material";
 
 const PreReading = () => {
   const { bookSelected } = useLibary();
+  const { user, setUser } = useUser();
 
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
 
   const navigate = useNavigate();
 
@@ -17,6 +37,25 @@ const PreReading = () => {
   const handleGoBack = () => {
     navigate(`/`);
   };
+
+  const isReading = user?.keepReading.reading.find(
+    (book) => book.id === bookSelected?.id
+  );
+
+  const deleteReading = ()=>{
+    const books = user?.keepReading.books.filter(
+      (book) => book.id !== bookSelected?.id
+    );
+    const readings = user?.keepReading.reading.filter(
+      (book) => book.id !== bookSelected?.id
+    );
+    setUser({...(user as IUser), keepReading:{
+      books: [...books as IDataBook[]],
+      reading: [...readings as IReading[]]
+    }})
+
+    handleCloseModal();
+  }
 
   return (
     <>
@@ -30,30 +69,104 @@ const PreReading = () => {
           mt: 8,
         }}
       >
-       <Book data={bookSelected as IDataBook}/>
+        <Book data={bookSelected as IDataBook} />
 
         <Box flexShrink={2}>
           <Description />
-          <Stack direction={"row"} justifyContent={"start"} spacing={3} mt={4}>
+          <Stack
+            direction={{ md: "row" }}
+            justifyContent={"start"}
+            mt={4}
+            gap={2}
+          >
+            <Button
+              size="large"
+              variant="contained"
+              color="primary"
+              onClick={handleRead}
+            >
+              {isReading ? "Continuar leitura" : "Começar a ler"}
+            </Button>
             <Button
               size="large"
               variant="contained"
               color="secondary"
-              onClick={handleRead}
-            >
-              Começar a ler
-            </Button>
-            <Button
-              size="large"
-              variant="outlined"
-              color="primary"
               onClick={handleGoBack}
             >
               Sair
             </Button>
+            {isReading && (
+              <Button
+                size="large"
+                variant="outlined"
+                color="warning"
+                onClick={handleOpenModal}
+              >
+                Excluir leitura
+              </Button>
+            )}
           </Stack>
         </Box>
       </Box>
+
+      <Modal
+        open={openModal}
+        onClose={handleCloseModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Container
+          maxWidth="xs"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          <Box
+            sx={{
+              position: "relative",
+              bgcolor: "Background",
+              borderRadius: 1,
+              boxShadow: 24,
+              p: 4,
+              textAlign: "center",
+            }}
+          >
+            <Typography variant="h5" color="primary">
+              Do you want to delete the reading?
+            </Typography>
+            <Typography variant="h6" color="error">
+              Your progress will be lost, and the information for this book will
+              be removed from the reports.
+            </Typography>
+            <Stack gap={4} direction={{ md: "row" }} mt={2}>
+              <Button fullWidth variant="contained" color="error" onClick={deleteReading}>
+                Delete
+              </Button>
+              <Button
+                fullWidth
+                variant="contained"
+                color="primary"
+                onClick={handleCloseModal}
+              >
+                Go back
+              </Button>
+            </Stack>
+            <IconButton
+              sx={{
+                position: "absolute",
+                top: 0,
+                right: 0,
+              }}
+              onClick={handleCloseModal}
+              aria-label="Close Access"
+            >
+              <Cancel fontSize="small" color="primary"></Cancel>
+            </IconButton>
+          </Box>
+        </Container>
+      </Modal>
     </>
   );
 };
