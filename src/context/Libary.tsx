0239826b1d85b8b1ba100d2f0d05f41fbd38ee2f
Matheus Aires <matheus.aires@majovdev.com.br>
2023-10-02 18:07:40 -0300

    Book selected state

--- src/context/Libary.tsx
+++ src/context/Libary.tsx
@@ -1,15 +1,35 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import data from "./../../data/books.json";
 
 export const LibaryContext = createContext<{
   collection: IDataBook[]; // Altere "any[]" para o tipo correto de sua coleção de dados
   searched: IDataBook[];
   setSearched: React.Dispatch<React.SetStateAction<IDataBook[]>>;
-
-}>({ collection: [], searched: [], setSearched: () => {} });
+  bookSelected: IDataBook | null;
+  setBookSelected: React.Dispatch<React.SetStateAction<IDataBook | null>>;
+}>({
+  collection: [],
+  searched: [],
+  setSearched: () => {},
+  bookSelected: null,
+  setBookSelected: () => {},
+});
 
 const Libary = ({ children }: { children: React.ReactNode }) => {
   const [searched, setSearched] = useState<IDataBook[]>([]);
+
+  const [bookSelected, setBookSelected] = useState<IDataBook | null>(() => {
+    const storedBook = localStorage.getItem("book");
+    return storedBook ? JSON.parse(storedBook) : null;
+  });
+
+  useEffect(() => {
+    if (bookSelected) {
+      localStorage.setItem("book", JSON.stringify(bookSelected));
+    } else {
+      localStorage.removeItem("book");
+    }
+  }, [bookSelected]);
 
   return (
     <LibaryContext.Provider
@@ -17,6 +37,8 @@ const Libary = ({ children }: { children: React.ReactNode }) => {
         collection: data,
         searched,
         setSearched,
+        bookSelected,
+        setBookSelected,
       }}
     >
       {children}
