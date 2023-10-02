c78a0bdb4646f7744028c5e364356a9939dd4c6b
Matheus Aires <matheus.aires@majovdev.com.br>
2023-10-02 18:08:28 -0300

    Book data props

--- src/components/Book/Book.tsx
+++ src/components/Book/Book.tsx
@@ -5,30 +5,41 @@ import {
   IconButton,
   Typography,
 } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useLibary } from "../../hooks/useLibary";
 
+const Book = ({ data }: { data: IDataBook }) => {
+  const navigate = useNavigate();
+  const { setBookSelected } = useLibary();
 
-const Book = ({
-  titulo = "Titulo do Livro",
-  autor = "Nome do Autor",
-  urlImage = "",
-}) => {
+  const handleCardClick = () => {
+    setBookSelected(data);
+
+    navigate(`/prereading`);
+  };
+
   return (
     <Card
+      onClick={handleCardClick}
       sx={{ maxWidth: 256, minHeight: 570, position: "relative", margin: 0 }}
     >
       <CardMedia
         component="img"
-        alt={titulo}
-        image={urlImage}
+        alt={data.name}
+        image={data.urlImage}
         loading="eager"
         sx={{ minHeight: 400 }}
       ></CardMedia>
       <CardContent>
         <Typography variant="body1" color="secondary" fontWeight="bolder">
-          {titulo.length > 25 ? `${titulo.substring(0, 25)}...` : titulo}
+          {data.name.length > 25
+            ? `${data.name.substring(0, 25)}...`
+            : data.name}
         </Typography>
         <Typography variant="body2" color="primary">
-          {autor.length > 50 ? `${autor.substring(0, 50)}...` : autor}
+          {data.description.length > 50
+            ? `${data.description.substring(0, 50)}...`
+            : data.description}
         </Typography>
       </CardContent>
 
